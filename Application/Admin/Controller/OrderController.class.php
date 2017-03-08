dd4b21a3aca05d1d7b76074d6597f4144dc04675
simpboy <simpboy@163.com>
2017-03-08 23:12:02 +0800

    发货

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -6,6 +6,20 @@ namespace Admin\Controller;
 use Common\Controller;
 
 class OrderController extends AdminController {
+	// 0-未发货，1-待发货，2-已发货，3-已收货
+	protected $express_state_map=[
+			0	=> '未发货',
+			1	=> '待发货',
+			2	=> '已发货',
+			3	=> '已收货',
+	];
+	protected function _initialize(){
+		parent::_initialize();
+		define('NOT_EXPRESS', 0);
+		define('FOR_EXPRESS', 1);
+		define('EXPRESSED', 2);
+		define('RECEIVE', 3);
+	}
     /**
      * 添加订单
      */
@@ -15,6 +29,9 @@ class OrderController extends AdminController {
         $Order_goods   	= M('order_goods','ngc_');
         $Address   		= M('address','ngc_');
         $Goods          = M('goods','ngc_');
+        if($order_id<=0){
+        	$this->error("参数错误！");
+        }
 
         $order_info		= $Order->where(['order_id'=>$order_id])->find();
         $order_goods	= $Order_goods->where(['order_id'=>$order_id])->select();
@@ -63,60 +80,34 @@ class OrderController extends AdminController {
         $this->display();
     }
 
-    /**
-     * 更改上下线
-     */
-    public function on_off_online()
-    {
-        $id = I('get.id');
-        $Goods = M('goods','ngc_');
-        $online = $Goods->where(['goods_id'=>$id])->getField('online');
-        $data = [
-            'online' => abs($online)-1
-        ];
-        $res = $Goods->where(['goods_id'=>$id])->save($data);
-        if($res!==false){
-            $this->success("操作成功");
-        }else{
-            $this->error("操作失败");
-        }
-    }
     /*
-     * 更改下单状态
+     * 
      */
-    public function on_off_state()
-    {
-        $id = I('get.id');
-        $Goods = M('goods','ngc_');
-        $state = $Goods->where(['goods_id'=>$id])->getField('state');
-        $data = [
-            'state' => abs($state)-1
-        ];
-        $res = $Goods->where(['goods_id'=>$id])->save($data);
-        if($res!==false){
-            $this->success("操作成功");
-        }else{
-            $this->error("操作失败");
-        }
-    }
-
-    public function del(){
-        $id = I('get.id');
-        $Goods = M('goods','ngc_');
-        $data = [
-            'del' => 1
-        ];
-        $res = $Goods->where(['goods_id'=>$id])->save($data);
-        if($res!==false){
-            $this->success("操作成功");
-        }else{
-            $this->error("操作失败");
-        }
-    }
-    public  function upload_img(){
-        $action = I('get.action','','trim');
-        $Up     = D('Admin/Up');
-        $Up->editor_upload($action);
+    public function edit_post(){
+    	$order_id = I('post.id',null,'intval');
+    	if(empty($order_id)){
+    		$this->error("参数错误");
+    	}
+    	$express_sn	 	= I("post.express_sn",'','trim');
+    	$express_name 	= I("post.express_name",'','trim');
+    	$update_express_time 	= I("post.update_express_time",0,'intval');
+    	
+    	$Order          = M('order','ngc_');
+    	
+    	$data = [
+    			'express_sn'	=> $express_sn,
+    			'express_name'  => $express_name,
+    			'express_state'	=> EXPRESSED
+    	];
+    	if($update_express_time==1){
+    		$data['express_time']	= NOW_TIME;
+    	}
+    	$res = $Order->where(['order_id'=>$order_id])->save($data);
+    	if($res!==false){
+    		$this->success("操作成功");
+    	}else{
+    		$this->error("操作失败");
+    	}
     }
 
 }
