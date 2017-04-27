ec7fa2798555c3452763d943184dac3d17c1fa92
simpman <simpman@163.com>
2017-04-27 15:28:25 +0800

    sql

--- Data/db/alter.sql
+++ Data/db/alter.sql
@@ -58,3 +58,20 @@ CREATE TABLE `ngc_category` (
   `online` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1-上线；0-下线',
   PRIMARY KEY (`cate_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+drop table if EXISTS `ngc_category`;
+CREATE TABLE `ngc_category` (
+  `cate_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+  `cate_name` varchar(200) NOT NULL DEFAULT '' COMMENT '分类名称',
+  `description` text NOT NULL COMMENT '描述',
+  `img` varchar(255) NOT NULL DEFAULT '' COMMENT '分类图片',
+  `parent_id` int(10) NOT NULL DEFAULT '0' COMMENT '父级分类id',
+  `display_order` int(5) NOT NULL DEFAULT '0' COMMENT '展示顺序desc',
+  `goods_number` int(10) NOT NULL DEFAULT '0' COMMENT '分类的商品数量',
+  `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间；',
+  `update_time` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否为异常数据；0-正常，-1-异常',
+  `online` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1-上线；0-下线',
+  `del` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0-没有删除；1-删除',
+  PRIMARY KEY (`cate_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
